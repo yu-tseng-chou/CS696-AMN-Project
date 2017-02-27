ae2c411fbaa8f7332836d418ced3e42b98230c6d
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-02-27 13:20:00 -0800

    [+] MedianDecomposition subplot

--- main.m
+++ main.m
@@ -11,14 +11,14 @@ for i=1:n
    
     
     figure
-    subplot(3,1,1);
+    subplot(4,1,1);
     imshow(im);
     title(sprintf('Original file %s', imname));
     
     G = rgb2gray(im);
     G = medfilt2(G);
     
-    subplot(3,1,2);
+    subplot(4,1,2);
     [counts,binLocations] = imhist(G);
     stem(binLocations, counts);
     title('Histogram of gray scale version image');
@@ -35,7 +35,7 @@ for i=1:n
     T = G;
     T(T > threshold) = 255;
     T(T <= threshold) = 0;
-    subplot(3,1,3);
+    subplot(4,1,3);
     imshow(T);
     title(sprintf('Filtered image with threshold = %d', threshold));
 
@@ -46,37 +46,42 @@ for i=1:n
     max_number = 10; % max number of songs
     indexes = zeros(3, max_number);
     
-    for n = 1:numel(rand_y)
-        y = rand_y(n);
-        count = 0;
-        thickness = 0;
-        index = 1;
-        for x = 1:size(T, 2)-1
-            
-            % reset thickness count if change detected
-            if (T(y,x) ~= T(y, x+1))
-                % if thickness count exceeds threshold, add the index of change
-                % to array
-                if thickness > thickness_threshold 
-                    count = count + 1;
-                    indexes(n, count) = index;                    
-                end
-                
-                index = x;
-                %lengths(y, count) = thickness;
-                thickness = 0;
-            end
-            
-            
-            thickness = thickness + 1;
-        end
-        
-        ind = indexes(n,:);
-        g=sprintf('%d, ', ind(ind>0));
-        %fprintf('Answer: %s\n', g);
-        subplot(3,1,3);
-        text(0, height+25*n, sprintf('Detected positions: %s', g));
-    end
+    subplot(4,1,4);
+    decomp = MedianDecomposition(T,3);
+    imshow(repmat(decomp,size(T,1),1));
+    title('After median decomposition of width=3');
+    
+%     for n = 1:numel(rand_y)
+%         y = rand_y(n);
+%         count = 0;
+%         thickness = 0;
+%         index = 1;
+%         for x = 1:size(T, 2)-1
+%             
+%             % reset thickness count if change detected
+%             if (T(y,x) ~= T(y, x+1))
+%                 % if thickness count exceeds threshold, add the index of change
+%                 % to array
+%                 if thickness > thickness_threshold 
+%                     count = count + 1;
+%                     indexes(n, count) = index;                    
+%                 end
+%                 
+%                 index = x;
+%                 %lengths(y, count) = thickness;
+%                 thickness = 0;
+%             end
+%             
+%             
+%             thickness = thickness + 1;
+%         end
+%         
+%         ind = indexes(n,:);
+%         g=sprintf('%d, ', ind(ind>0));
+%         %fprintf('Answer: %s\n', g);
+%         subplot(3,1,3);
+%         text(0, height+25*n, sprintf('Detected positions: %s', g));
+%     end
     
 
 end
