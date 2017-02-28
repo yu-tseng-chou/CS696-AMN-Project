6042cf48ebf3d8c1e00d47748fd184fdeea223a5
Jenny Chou <Jenny@Jennys-MacBook-Pro.local>
2017-02-27 18:31:08 -0800

    [+] SongLegnthExtraction subplot and auto-detected threshold

--- main.m
+++ main.m
@@ -1,88 +1,85 @@
-clc;clear;clf;close all;
 % Clean up
+clc;clear;clf;close all;
 
+% Loading the images
 filelist = dir('images/Strip*.jpg');
-m = 4;
 n = length(filelist);
+
 for i=1:n
     %% Load the original image
     imname = filelist(i).name;
 	im = imread(['images/' imname], 'jpg');
    
-    
     figure
     subplot(4,1,1);
     imshow(im);
     title(sprintf('Original file %s', imname));
     
+    %% Filter image with a median filter and convert it to gray scale
     G = rgb2gray(im);
     G = medfilt2(G);
     
+    %% Get the optimal threshold for image filtering
     subplot(4,1,2);
     [counts,binLocations] = imhist(G);
     stem(binLocations, counts);
     title('Histogram of gray scale version image');
     
-%     % TO DO: get threshold from between peaks
-%     first_peak = 0;
-%     for ind = 1:256
-%         if counts(ind) > first_peak
-%             first_peak = counts(ind);
-%         end
-%     end
+    % TO DO: get threshold from between peaks
+    first_peak_value = 0;
+    first_peak_index = 0;
+    second_peak_value = 0;
+    second_peak_index = 0;
+    index = 0;
+    for ind = 2:255
+        if ((counts(ind) > first_peak_value || counts(ind) > second_peak_value) ...
+            && counts(ind-1) < counts(ind) && counts(ind) > counts(ind+1))
+            
+            second_peak_value = counts(ind);
+            second_peak_index = ind;
+            if (second_peak_value > first_peak_value)
+                temp_index = first_peak_index;
+                temp_value = first_peak_value;
+                
+                first_peak_index = second_peak_index;
+                first_peak_value = second_peak_value;
+                
+                second_peak_index = temp_index;
+                second_peak_value = temp_value;
+            end
+        end
+    end
     
-    threshold = 30; %(first_peak + second_peak))/2;
+    min_index = 1;
+    min_value = max(counts);
+    for ind = first_peak_index : sign(second_peak_index-first_peak_index): second_peak_index
+        if (counts(ind) < min_value)
+            min_index = ind;
+            min_value = counts(ind);
+        end
+    end
+    
+    %% Filter image with a threshold so only black and white left
+    threshold = min_index; % (first_peak_index + second_peak_index)/2;
     T = G;
     T(T > threshold) = 255;
     T(T <= threshold) = 0;
     subplot(4,1,3);
     imshow(T);
     title(sprintf('Filtered image with threshold = %d', threshold));
-
     
-    height = size(T,1);
-    rand_y = randperm(height, 3);
-    thickness_threshold = 2;
-    max_number = 10; % max number of songs
-    indexes = zeros(3, max_number);
-    
+    %% 
     subplot(4,1,4);
     filterWidth=3;
     decomp = MedianDecomposition(T,filterWidth);
     imshow(repmat(decomp,size(T,1),1));
     title(sprintf('After median decomposition of width = %d', filterWidth));
     
-%     for n = 1:numel(rand_y)
-%         y = rand_y(n);
-%         count = 0;
-%         thickness = 0;
-%         index = 1;
-%         for x = 1:size(T, 2)-1
-%             
-%             % reset thickness count if change detected
-%             if (T(y,x) ~= T(y, x+1))
-%                 % if thickness count exceeds threshold, add the index of change
-%                 % to array
-%                 if thickness > thickness_threshold 
-%                     count = count + 1;
-%                     indexes(n, count) = index;                    
-%                 end
-%                 
-%                 index = x;
-%                 %lengths(y, count) = thickness;
-%                 thickness = 0;
-%             end
-%             
-%             
-%             thickness = thickness + 1;
-%         end
-%         
-%         ind = indexes(n,:);
-%         g=sprintf('%d, ', ind(ind>0));
-%         %fprintf('Answer: %s\n', g);
-%         subplot(3,1,3);
-%         text(0, height+25*n, sprintf('Detected positions: %s', g));
-%     end
+    %% Extract song length information
+    height = size(T,1);
+    song_lengths = SongLengthsExtraction(decomp);
+    g = sprintf('%d, ', song_lengths);
+    g = g(1:size(g,2)-2); % Trim off trailing comma
+    text(0, height+25, sprintf('%d Songs detected. Lengths: %s', size(song_lengths, 2), g));
     
-
 end
