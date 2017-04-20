f2ae0ed45c5b9a93ea84ca8c745ce002827315ab
Kevin O'Mara <kevin.d.omara@gmail.com>
2017-04-20 10:20:48 -0700

    Correctly count silent grooves
    The track-separator sections (wide black region) sometimes have a
    white silent groove. This groove was being incorrectly counted
    toward the next song, not the previous song.
    
    This fix checks for this silent groove, and correctly attributes it
    to the previous song.

--- SongLengthsExtraction.m
+++ SongLengthsExtraction.m
@@ -2,10 +2,11 @@ function song_lengths = SongLengthsExtraction(decomp)
 % Return vector with number of grooves in each song.
 %   decomp - a 1-D array of black/white (median filtered image)
 
-    % set minimum width to detect change of song number 
-    threshold       = 15;
+    threshold       = 15;   % minimum width to detect change of song
+    min_threshold   = 10;   % minimum width to detect final silent groove
     song_lengths    = [];
     
+    %  0 = black, 1 = white
     % [00011100110011000000...] decomp
     % [00111001100110000000...] shifted_decomp (<<1)
     % [00100001000100000000...] 
@@ -15,17 +16,28 @@ function song_lengths = SongLengthsExtraction(decomp)
     groove_count    = 1;
     
     % count number of grooves per song
-    for n = 1: size(detected, 2)-1
+    n = 1;
+    while (n <= size(detected, 2)-1)
         distance_between = detected(n+1) - detected(n);
-        if  distance_between > threshold
-            % found end of song
+        
+        % silent groove in song separator
+        if groove_count == 1 && distance_between > min_threshold
+            song_lengths(numel(song_lengths)) = ...
+                song_lengths(numel(song_lengths)) + 1;
+            groove_count    = 0;
+
+        % end of song
+        elseif  distance_between > threshold
             song_lengths(numel(song_lengths)+1) = groove_count;
-            groove_count        = 0;
+            groove_count	= 0;
         end
         
         groove_count     = groove_count + 1;
+        n                = n + 1;
     end
     
-    % include final song
-    song_lengths(numel(song_lengths)+1) = groove_count;
+    % include final song, but not final silent groove
+    if groove_count > 1
+        song_lengths(numel(song_lengths)+1) = groove_count;
+    end
 end
